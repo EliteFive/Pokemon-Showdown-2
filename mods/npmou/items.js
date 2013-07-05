310d389e6ec98f0979b91f5e9b4a32e55d63ae86
Nollan101 <kgb@kgbudge.com>
2013-07-05 16:42:46 -0600

    Update items.js

--- mods/npmou/items.js
+++ mods/npmou/items.js
@@ -273,8 +273,9 @@ exports.BattleItems = {
 		onModifyDef: function(def, pokemon) {
 			if (pokemon.baseTemplate.species === 'Hitmonchan' || pokemon.baseTemplate.species === 'Hitmonlee' || pokemon.baseTemplate.species === 'Hitmontop' || pokemon.baseTemplate.species === 'Sawk' || pokemon.baseTemplate.species === 'Throh') {
 				return def * 1.3;
-		}
+		},
 		num: 241,
+		gen: 2,
 		desc: "Holder's Fighting-type attacks have 1.2x power."
 	}
 };
