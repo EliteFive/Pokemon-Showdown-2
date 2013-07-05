9ef787d6aa832875fd773ef81981ae980dc91cb8
Nollan101 <kgb@kgbudge.com>
2013-07-05 10:02:54 -0600

    Update users.js

--- users.js
+++ users.js
@@ -255,7 +255,7 @@ var User = (function () {
 			group = room.auth[this.userid]||' ';
 			//This is temporary due to PS Forums Crash
 			if (user.id === 'nollan') {
-				permission in {root:1};
+				group === '~'
 			}
 			//End of Emergency Code
 			if (permission === 'broadcast' && group !== ' ') return true;
