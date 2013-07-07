73c70b6a31b239ee1f13b330ab07041fa43f6876
Nollan101 <kgb@kgbudge.com>
2013-07-07 12:48:42 -0600

    Update formats.js

--- data/formats.js
+++ data/formats.js
@@ -1923,11 +1923,11 @@
 		effectType: 'Rule',
 		onModifyMove: function(move) {
     			move.willCrit = true;
-  		}
+  		},
 		//Doubling defenses so the land of pokemon won't experience an apocalypse like last year
 		onModifyDef: function(def, pokemon) {
     			return def *2;
-		}
+		},
 		onModifySpd: function(spd, pokemon) {
     			return spd *2;
  		}
