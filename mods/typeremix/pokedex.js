4ea202669255c885eac59e417efaef997108b82e
Nollan101 <kgb@kgbudge.com>
2013-07-08 13:10:37 -0600

    Mild testing

---
+++ mods/typeremix/pokedex.js
@@ -0,0 +1,3 @@
+exports.BattlePokedex = {
+kyuremblack:{num:646,species:"Kyurem-Black",baseSpecies:"Kyurem",forme:"Black",formeLetter:"B",types:["Dragon","Electric"],gender:"N",baseStats:{hp:125,atk:170,def:100,spa:120,spd:90,spe:95},abilities:{0:"Teravolt"},heightm:3.3,weightkg:325,color:"Gray",eggGroups:["No Eggs"]},
+};
