5c2c89bbf0f930029e41040e42b36aa71055fced
Nollan101 <kgb@kgbudge.com>
2013-07-08 14:19:45 -0600

    Update pokedex.js

--- mods/typeremix/pokedex.js
+++ mods/typeremix/pokedex.js
@@ -1,3 +1,3 @@
 exports.BattlePokedex = {
-charizard:{num:6,species:"Charizard",types:["Dragon","Water"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:78,atk:84,def:78,spa:109,spd:85,spe:100},abilities:{0:"Blaze",DW:"Solar Power"},heightm:1.7,weightkg:90.5,color:"Red",prevo:"charmeleon",evoLevel:36,eggGroups:["Monster","Dragon"]},
+charizard:{num:6,species:"Charizard",types:["Ice","Poison"],genderRatio:{M:0.875,F:0.125},baseStats:{hp:78,atk:84,def:78,spa:109,spd:85,spe:100},abilities:{0:"Blaze",DW:"Solar Power"},heightm:1.7,weightkg:90.5,color:"Red",prevo:"charmeleon",evoLevel:36,eggGroups:["Monster","Dragon"]},
 };
